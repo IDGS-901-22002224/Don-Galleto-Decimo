76396c36df8e36143a3bb51c72b0c766fcc329a5
Oscar-Gomez-Luna <oscardio.lunagomez@gmail.com>
2025-10-27 22:47:58 -0600

    Vaciado de main

--- static/js/modal.js
+++
@@ -1,9 +0,0 @@
-function abrirModal() {
-    const modal = document.getElementById('modalReceta');
-    modal.style.display = 'flex';
-}
-
-function cerrarModal() {
-    const modal = document.getElementById('modalReceta');
-    modal.style.display = 'none';
-}
